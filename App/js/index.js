eb3b5bb2d1ec4a09ea17298ad3a69b129ded813b
Alejandrix2456github <126512358+Alejandrix2456github@users.noreply.github.com>
2024-01-07 13:19:57 -0600

    Updated by CrossUI No-Code App Builder

--- App/js/index.js
+++ App/js/index.js
@@ -39,36 +39,61 @@ xui.Class('App', 'xui.Module',{
             );
             
             host.ctl_layout5.append(
-                xui.create("xui.UI.Layout")
-                .setHost(host,"ctl_layout13")
+                xui.create("xui.UI.Image")
+                .setHost(host,"xui_ui_image4")
+                .setLeft("0.7619047619047619em")
+                .setTop("0em")
+                .setSrc("https://static2.yooco.de/n2/cd/788547/images/logo/orig/c2dc70c959be5450ab6c4e1e75dd2faa.png"),
+                "before"
+            );
+            
+            host.ctl_layout5.append(
+                xui.create("xui.UI.ToolBar")
+                .setHost(host,"xui_ui_toolbar6")
                 .setItems([
                     {
-                        "id" : "before",
-                        "pos" : "before",
-                        "size" : 150,
-                        "min" : 10,
-                        "locked" : false,
-                        "folded" : false,
-                        "hidden" : false,
-                        "cmd" : true
+                        "id" : "grp1",
+                        "sub" : [
+                            {
+                                "id" : "a1",
+                                "caption" : "Home"
+                            },
+                            {
+                                "id" : "a2",
+                                "type" : "split"
+                            },
+                            {
+                                "id" : "a3",
+                                "caption" : "Members",
+                                "type" : "dropButton"
+                            },
+                            {
+                                "id" : "a4",
+                                "caption" : "status button",
+                                "type" : "statusButton"
+                            }
+                        ],
+                        "caption" : "grp1"
                     },
                     {
-                        "id" : "main",
-                        "min" : 10,
-                        "size" : 530
-                    },
-                    {
-                        "id" : "after",
-                        "pos" : "after",
-                        "size" : 120,
-                        "min" : 10,
-                        "locked" : false,
-                        "folded" : false,
-                        "hidden" : false,
-                        "cmd" : false
+                        "id" : "grp2",
+                        "sub" : [
+                            {
+                                "id" : "b1",
+                                "imageClass" : "xui-icon-xui",
+                                "caption" : ""
+                            },
+                            {
+                                "id" : "b2",
+                                "caption" : "image button",
+                                "label" : "label:",
+                                "imageClass" : "xui-icon-xui"
+                            }
+                        ],
+                        "caption" : "grp2"
                     }
                 ])
-                .setType("horizontal"),
+                .setTop("3.8095238095238093em"),
                 "main"
             );
             
